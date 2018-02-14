eba123ac6825dd381cdd5d521af6db0c564c9b31
Jamila Keba <jamilaleilanikeba@gmail.com>
2018-02-14 13:28:15 -0800

    added styling and API call

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 $(function() {
   console.log("JS working")
 
+  $("main").css("height", $(window).height());
+
+  $("#womanquote").css("height", $("#womanquote").width());
+  console.log($("#womanquote").width());
+
   $("#quotespage").hide();
 
   $(function() {
@@ -12,13 +17,31 @@ $(function() {
     });
   });
 
+  $("button").on("click", function() {
 
-  var womanimage = '"img/cat.png"'
-  var womanquote = "Meow meow meowwww"
-  var womanname = "Cat"
+    var compliment_endpoint="https://catalyst-api.herokuapp.com/quotes";
+    $.ajax({
+      method: "GET",
+      url: compliment_endpoint,
+      data: {
+      },
+      success: function(response) {
+          var randomcompliment = Math.floor((Math.random()*response.length)+1);
+          var womanname = response[randomcompliment].name;
+          var womanquote = response[randomcompliment].quote;
+          var womanimage = response[randomcompliment].image_url;
 
-  $("#womanimage").html(`<img src=${womanimage}>`);
-  $("#womanquote").html(`"${womanquote}" <span> -${womanname}</span>`);
+          $("#womanimage").html(`<img src=${womanimage}>`);
+          $("#womanquote").html(`<span>"${womanquote}"</span> <span id="quoteauthor"> -${womanname}</span>`);
+         },
+
+        error: function(response){
+          console.log('Dang it!');
+        }
+    });
+
+  });
+
 
 });
 
